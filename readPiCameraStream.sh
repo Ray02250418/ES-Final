5bc1b67d592dd6404e7133024923069de0ba1f64
b07502089 <a6140000@gmail.com>
2022-01-07 15:38:50 +0800

    modify to 2 camera

--- readPiCameraStream.sh
+++ readPiCameraStream.sh
@@ -1,7 +1,14 @@
-rm -rf picamera
-if [ -p picamera ]
+rm -rf picamera1
+rm -rf picamera2
+if [ -p picamera1 ]
 then 
-    rm -rf picamera
+    rm -rf picamera1
 fi
-mkfifo picamera
-nc -l 8081 -v > picamera
+if [ -p picamera2 ]
+then 
+    rm -rf picamera2
+fi
+mkfifo picamera1
+mkfifo picamera2
+nc -l 8080 -v > picamera1 &
+nc -l 8081 -v > picamera2

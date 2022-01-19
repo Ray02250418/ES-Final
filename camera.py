9bf52b159466ac5b0b5ed7e1c1a7a65dad08c6dd
Ray02250418 <s960379ray@gmail.com>
2022-01-19 22:18:37 +0800

    Finish using thread

---
+++ camera.py
@@ -0,0 +1,43 @@
+import socket, pickle, struct
+import numpy as np
+import cv2
+from inference import detection_model
+
+class Camera():
+    def __init__(self, ip, port, num):
+        # create socket
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((ip, port))  # a tuple
+        self.data = b""
+        self.payload_size = struct.calcsize("Q") # Q: unsigned long long integer(8 bytes)
+        self.num = num
+
+        # create inference self.model
+        self.model = detection_model('Sample_TFLite_model', 0.5)
+
+    def detect(self):
+        #Business logic to receive self.data frames, and unpak it and de-serialize it and show video frame on client side
+        while True:
+            while len(self.data) < self.payload_size:
+                packet = self.client_socket.recv(4 * 1024)  # 4K, range(1024 byte to 64KB)
+                if not packet: break
+                self.data += packet # append the self.data packet got from server into self.data variable
+            packed_msg_size = self.data[:self.payload_size] #will find the packed message size i.e. 8 byte, we packed on server side.
+            self.data = self.data[self.payload_size:] # Actual frame self.data
+            msg_size = struct.unpack("Q", packed_msg_size)[0] # meassage size
+            
+            while len(self.data) < msg_size:
+                self.data += self.client_socket.recv(4 * 1024) # will receive all frame self.data from client socket
+            frame_data = self.data[:msg_size] #recover actual frame self.data
+            self.data = self.data[msg_size:]
+            frame = pickle.loads(frame_data) # de-serialize bytes into actual frame type
+
+            (boxes, classes, scores, labels) = self.model.inference(frame) # inference self.model
+            print('------------------------------------------------------------------------')
+            print(' ' * self.num * 100, self.num, ': ', labels)
+            #inference_frame(frame, self.model)
+
+            # cv2.imshow("RECEIVING VIDEO", frame) # show video frame at client side
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'): # press q to exit video
+                break
--- client.py
+++ client.py
@@ -1,90 +1,13 @@
-import socket, pickle, struct
-import numpy as np
-import cv2
-from inference import detection_model
+from camera import Camera
+from multiprocessing import Process
 from threading import Thread
 
-
-def camera1():
-    # create socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host_ip = '192.168.50.86'  # paste your server ip address here
-    port = 9999
-    client_socket.connect((host_ip, port))  # a tuple
-    data = b""
-    payload_size = struct.calcsize("Q") # Q: unsigned long long integer(8 bytes)
-
-    # create inference model
-    model = detection_model('Sample_TFLite_model', 0.5)
-
-
-    #Business logic to receive data frames, and unpak it and de-serialize it and show video frame on client side
-    while True:
-        while len(data) < payload_size:
-            packet = client_socket.recv(4 * 1024)  # 4K, range(1024 byte to 64KB)
-            if not packet: break
-            data += packet # append the data packet got from server into data variable
-        packed_msg_size = data[:payload_size] #will find the packed message size i.e. 8 byte, we packed on server side.
-        data = data[payload_size:] # Actual frame data
-        msg_size = struct.unpack("Q", packed_msg_size)[0] # meassage size
-        
-        while len(data) < msg_size:
-            data += client_socket.recv(4 * 1024) # will receive all frame data from client socket
-        frame_data = data[:msg_size] #recover actual frame data
-        data = data[msg_size:]
-        frame = pickle.loads(frame_data) # de-serialize bytes into actual frame type
-
-        (boxes, classes, scores, labels) = model.inference(frame) # inference model
-        print('--------------------------------------------------------------------------------------------------')
-        print(' '*100, labels)
-        #inference_frame(frame, model)
-
-        # cv2.imshow("RECEIVING VIDEO", frame) # show video frame at client side
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'): # press q to exit video
-            break
-        
-def camera2():
-    # create socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host_ip = '192.168.50.243'  # paste your server ip address here
-    port = 9999
-    client_socket.connect((host_ip, port))  # a tuple
-    data = b""
-    payload_size = struct.calcsize("Q") # Q: unsigned long long integer(8 bytes)
-
-    # create inference model
-    model = detection_model('Sample_TFLite_model', 0.5)
-
-
-    #Business logic to receive data frames, and unpak it and de-serialize it and show video frame on client side
-    while True:
-        while len(data) < payload_size:
-            packet = client_socket.recv(4 * 1024)  # 4K, range(1024 byte to 64KB)
-            if not packet: break
-            data += packet # append the data packet got from server into data variable
-        packed_msg_size = data[:payload_size] #will find the packed message size i.e. 8 byte, we packed on server side.
-        data = data[payload_size:] # Actual frame data
-        msg_size = struct.unpack("Q", packed_msg_size)[0] # meassage size
-        
-        while len(data) < msg_size:
-            data += client_socket.recv(4 * 1024) # will receive all frame data from client socket
-        frame_data = data[:msg_size] #recover actual frame data
-        data = data[msg_size:]
-        frame = pickle.loads(frame_data) # de-serialize bytes into actual frame type
-
-        (boxes, classes, scores, labels) = model.inference(frame) # inference model
-        print('--------------------------------------------------------------------------------------------------')
-        print(labels)
-        #inference_frame(frame, model)
-
-        # cv2.imshow("RECEIVING VIDEO2", frame) # show video frame at client side
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'): # press q to exit video
-            break
-        
-right = Thread(target=camera1)
-left = Thread(target=camera2)
-
-right.start()
-left.start()
+if __name__ == '__main__':
+    camera0 = Camera('192.168.50.243', 5555, 0)
+    camera1 = Camera('192.168.50.86', 8889, 1)
+    Left = Thread(target=camera0.detect)
+    Right = Thread(target=camera1.detect)
+    Left.start()
+    Right.start()
+    
+    

973d1412c02bbad1a5fe64842053fac7c4c78478
Ray02250418 <s960379ray@gmail.com>
2022-01-20 15:57:53 +0800

    temp

--- client.py
+++ client.py
@@ -1,13 +1,27 @@
 from camera import Camera
-from multiprocessing import Process
+from multiprocess import Process, Value 
 from threading import Thread
 
-if __name__ == '__main__':
+def setup_camera0(danger):
     camera0 = Camera('192.168.50.243', 5555, 0)
+    camera0.detect(danger)
+    
+def setup_camera1(danger):
     camera1 = Camera('192.168.50.86', 8889, 1)
-    Left = Thread(target=camera0.detect)
-    Right = Thread(target=camera1.detect)
+    camera1.detect(danger)
+    
+if __name__ == '__main__':
+    # camera0 = Camera('192.168.50.243', 5555, 0)
+    # camera1 = Camera('192.168.50.86', 8889, 1)
+    left_danger = Value('i', 0)
+    right_danger = Value('i', 0)
+    Left = Process(target=setup_camera0, args=[left_danger])
+    Right = Process(target=setup_camera1, args=[right_danger])
     Left.start()
     Right.start()
+    
+    
+    # left_danger = 0
+    # setup_camera(left_danger, '192.168.50.243', 5555, 0)
     
     

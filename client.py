f5dbab5575fd53f13cd1d2268f4ce6416090fd17
b07502089 <a6140000@gmail.com>
2022-01-20 15:57:28 +0800

    add mp

--- client.py
+++ client.py
@@ -1,13 +1,15 @@
 from camera import Camera
-from multiprocessing import Process
+import multiprocessing as mp
 from threading import Thread
 
 if __name__ == '__main__':
     camera0 = Camera('192.168.50.243', 5555, 0)
     camera1 = Camera('192.168.50.86', 8889, 1)
-    Left = Thread(target=camera0.detect)
-    Right = Thread(target=camera1.detect)
+    Left = mp.Process(target=camera0.detect)
+    Right = mp.Process(target=camera1.detect)
     Left.start()
     Right.start()
     
+    Left.join()
+    Right.join()
     
